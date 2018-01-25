fc095131a3907c3a6cf5411119997d47b5e73c17
Hiroshi Kikuchi <hiroshi.kikuchi@mixi.co.jp>
2018-01-25 15:44:17 +0900

    Show map in Map tab

---
+++ DroidKaigi2018/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  DroidKaigi2018
+//
+//  Created by hiroshi.kikuchi on 2018/01/25.
+//  Copyright © 2018年 DroidKaigi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end

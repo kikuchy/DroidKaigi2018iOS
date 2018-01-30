0f7c34733a7b7f7c570c95ab57a191c73400d366
Hiroshi Kikuchi <hiroshi.kikuchi@mixi.co.jp>
2018-01-30 19:48:28 +0900

    Set colors for status bar and so on.

--- src/main/kotlin/AppDelegate.kt
+++ src/main/kotlin/AppDelegate.kt
@@ -14,6 +14,15 @@ class AppDelegate : UIResponder(), UIApplicationDelegateProtocol {
     override fun application(application: UIApplication, didFinishLaunchingWithOptions: NSDictionary?): Boolean {
         val key = NSBundle.mainBundle.objectForInfoDictionaryKey("GoogleMapsAPIKey")?.uncheckedCast<NSString>().toString()
         GMSServices.provideAPIKey(key)
+
+        UINavigationBar.appearance().apply {
+            barTintColor = UIColor(red = 251.0 / 256.0, green = 200.0 / 256.0, blue = 18.0 / 256.0, alpha = 1.0)
+            tintColor = UIColor.whiteColor
+        }
+        UITabBar.appearance().apply {
+            barTintColor = UIColor(red = 251.0 / 256.0, green = 200.0 / 256.0, blue = 18.0 / 256.0, alpha = 1.0)
+            tintColor = UIColor.whiteColor
+        }
         return true
     }
 
